cc9ef849108e66f8e27ad3852ffd7896f5d0ff28
Alex <alexmitchellvball@gmail.com>
2015-09-22 22:28:44 -0600

    FINISHED

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -9,22 +9,161 @@ Token addToken(string type, string content, int lineCount)
 {
 	Token tokenAdd;
 	tokenAdd.setType(type);
-	tokenAdd.setContent(",");
+	tokenAdd.setContent(content);
 	tokenAdd.setLineNumber(lineCount);
 	return tokenAdd;
 }
-
 
 string removeFromString(string content, int numToRemove)
 {
 	return content.erase(0, numToRemove);
 }
 
+Token genID(string &content, int &lineCount)
+{
+	string idContent = "";
+	idContent.push_back(content[0]);
+	int i = 1;
+	while (isalnum(content[i]))
+	{
+		idContent.push_back(content[i]);
+		i++;
+	}
+
+	content = removeFromString(content, idContent.length());
+	return addToken("ID", idContent, lineCount);
+}
+
+bool isKeyword(string content, string toCompare)
+{
+	char lastCheck = 0; 
+
+	if(content.size() < toCompare.size())
+	{
+		return false;
+	}
+
+	for(auto i = 0; i < toCompare.size(); i++)
+	{
+		if (toCompare[i] != content[i])
+		{
+			return false;
+		}
+		lastCheck = content[i+1];
+	}
+
+	if (!isspace(lastCheck))
+	{
+		return false;
+	}
+	
+
+	return true;
+}
+
+Token genUndefToken(string content, int lineNumber)
+{
+	return addToken("UNDEFINED", content, lineNumber);
+}
+
+Token interpretString(string &content, int &lineCount)
+{
+	string stringResult = "\'";
+	int stringStartLine = lineCount;
+	int i = 1;
+	while (true)
+	{
+		if (i == content.length())
+		{
+			content = "";
+			return genUndefToken(stringResult, stringStartLine);
+		}
+
+ 		if (content[i] == '\'')
+		{
+			if (content[i+1] != '\'')
+			{
+				stringResult.push_back(content[i]);
+				break;
+			}
+			else
+			{
+				stringResult.push_back(content[i++]);
+			}
+		}
+
+		if (content[i] == '\n')
+		{
+			lineCount++;
+		}
+
+		stringResult.push_back(content[i]);
+
+		if (content[i] =='$')
+		{
+			return genUndefToken(stringResult, stringStartLine);
+		}
+				
+		i++;
+	}
+
+	content = removeFromString(content, stringResult.length());
+	return addToken("STRING", stringResult, stringStartLine);
+}
+
+Token genMultiLineComment(string &content, int &lineCount)
+{
+	string commentContent = "#|";
+	int commentStartLineCount = lineCount;
+	int i = 2;
+
+	while (true)
+	{
+		if (content.size() == 0)
+		{
+			return genUndefToken(commentContent, commentStartLineCount);
+		}
+
+		if (content[i] == '|')
+		{
+			if (content[i+1] == '#')
+			{
+				commentContent.append("|#");
+				break;
+			}
+		}
+
+		if (content[i] =='\n')
+		{
+			lineCount++;
+		}
+
+		commentContent.push_back(content[i]);
+		i++;
+	}
+
+	content = removeFromString(content, commentContent.size());
+	return addToken("COMMENT", commentContent, commentStartLineCount);
+}
+
+Token genSingleLineComment(string &content, int lineCount)
+{
+	int i = 0;
+	string comment;
+	while (content[i] != '\n' && content[i] != '$')
+	{
+		comment.push_back(content[i]);
+		i++;
+	}
+	content = removeFromString(content, i + 1);
+	return addToken("COMMENT", comment, lineCount);
+}
+
 vector<Token> LexicalAnalysis(string content)
 {
 	vector<Token> result;
 	int lineCount = 1;
-	while (content.size() != 0)
+	while (content.size() > 0)
 	{
 		Token toAdd;
 		char nextChar = content[0];
@@ -86,26 +225,105 @@ vector<Token> LexicalAnalysis(string content)
 			toAdd = addToken("WHITESPACE", "\\n", lineCount);
 			content = removeFromString(content, 1);
 			lineCount++;
-			result.push_back(toAdd);
 			break;
-		case '\s':
+		case ' ':
 			toAdd = addToken("WHITESPACE", "\\s", lineCount);
 			content = removeFromString(content, 1);
-			result.push_back(toAdd);
 			break;
 		case '\t':
 			toAdd = addToken("WHITESPACE", "\\t", lineCount);
 			content = removeFromString(content, 1);
+			break;
+		case 'F':
+			if (isKeyword(content, "Facts"))
+			{
+				toAdd = addToken("FACTS", "Facts", lineCount);
+				content = removeFromString(content, 5);
+				result.push_back(toAdd);
+			}
+			else
+			{
+				toAdd = genID(content, lineCount);
+				result.push_back(toAdd);
+			}
+			break;
+		case 'S':
+			if (isKeyword(content, "Schemes"))
+			{
+				toAdd = addToken("SCHEMES", "Schemes", lineCount);
+				content = removeFromString(content, 7);
+				result.push_back(toAdd);
+			}
+			else
+			{
+				toAdd = genID(content, lineCount);
+				result.push_back(toAdd);
+			}
+			break;
+		case 'Q':
+			if (isKeyword(content, "Queries"))
+			{
+				toAdd = addToken("QUERIES", "Queries", lineCount);
+				content = removeFromString(content, 7);
+				result.push_back(toAdd);
+			}
+			else
+			{
+				toAdd = genID(content, lineCount);
+				result.push_back(toAdd);
+			}
+			break;
+		case 'R':
+			if (isKeyword(content, "Rules"))
+			{
+				toAdd = addToken("RULES", "Rules", lineCount);
+				content = removeFromString(content, 5);
+				result.push_back(toAdd);
+			}
+			else
+			{
+				toAdd = genID(content, lineCount);
+				result.push_back(toAdd);
+			}
+			break;
+		case '#':
+			if (content[1] != '|')
+			{
+				toAdd = genSingleLineComment(content, lineCount);
+				result.push_back(toAdd);
+				lineCount++;
+			}
+			else
+			{
+				toAdd = genMultiLineComment(content, lineCount);
+				result.push_back(toAdd);
+			}
+			break;
+		case '\'':
+			toAdd = interpretString(content, lineCount);
 			result.push_back(toAdd);
 			break;
 		default:
-			toAdd = addToken("FIX", "FIX", lineCount);
-			content = removeFromString(content, 1);
-			result.push_back(toAdd);
+			if (isalpha(nextChar))
+			{
+				toAdd = genID(content, lineCount);
+				result.push_back(toAdd);
+			}
+			else 
+			{
+				string undefinedChar = "";
+				undefinedChar.push_back(nextChar);
+				toAdd = genUndefToken(undefinedChar, lineCount);
+				content = removeFromString(content, 1);
+				result.push_back(toAdd);
+			}
 			break;
 		}
 
 	}
+
+	Token toAdd = addToken("EOF", "", lineCount);
+	result.push_back(toAdd);
 	return result;
 	
 }
@@ -124,12 +342,11 @@ string readFile(string filename)
 
 	string content;
 
-	while (!fin.eof())
+	char line[256];
+	while (fin.getline(line, 256))
 	{
-		char line[256];	
-		fin.getline(line, 256);
 		content.append(line);
-		content.append("\n");
+		content.push_back('\n');
 	}
 
 	cout << content;
